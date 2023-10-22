5e5fc4b1a0794b0b320b6a74bbecd20a8718128a
Gayoon1234 <s3843584@student.rmit.edu.au>
2023-10-22 20:05:18 +1100

    added base project and colors

---
+++ constants/Colors.ts
@@ -0,0 +1,19 @@
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#DDA15E"; //tint is like active color
+
+export default {
+  light: {
+    text: "#000",
+    background: "#fff",
+    tint: tintColorLight,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#FEFAE0",
+    background: "#283618",
+    tint: tintColorDark,
+    tabIconDefault: "#283618",
+    tabIconSelected: tintColorDark,
+  },
+};
