7a739cbbb710c6407c1595e99be20ca4279517ca
Gayoon1234 <s3843584@student.rmit.edu.au>
2023-11-16 21:31:52 +1100

    added export and privacy policy

--- components/helper/MoreModal.tsx
+++ components/helper/MoreModal.tsx
@@ -6,9 +6,13 @@ import {
   Pressable,
   StatusBar,
   FlatList,
+  Clipboard,
 } from "react-native";
 import Colors from "../../constants/Colors";
 import { FontAwesome } from "@expo/vector-icons";
+import { useState } from "react";
+import CustomModal from "./CustomModal";
+import { useSaveData } from "../../app/contexts/SaveDataContext";
 
 interface MoreModalProps {
   visible: boolean;
@@ -19,10 +23,15 @@ interface SettingsOptionProps {
   id: string;
   title: string;
   icon: string;
+  settingsFunction: () => void;
 }
 
-const SettingsOption: React.FC<SettingsOptionProps> = ({ title, icon }) => (
-  <View style={styles.settingsOptionContainer}>
+const SettingsOption: React.FC<SettingsOptionProps> = ({
+  title,
+  icon,
+  settingsFunction,
+}) => (
+  <Pressable onPress={settingsFunction} style={styles.settingsOptionContainer}>
     <FontAwesome
       //@ts-ignore
       name={icon}
@@ -33,19 +42,68 @@ const SettingsOption: React.FC<SettingsOptionProps> = ({ title, icon }) => (
       size={40}
     />
     <Text style={styles.text}>{title}</Text>
-  </View>
+  </Pressable>
 );
 
 const MoreModal: React.FC<MoreModalProps> = ({ visible, setVisible }) => {
+  const [visible2, setVisible2] = useState(false);
+  const [modalText, setModalText] = useState("");
+  const { saveData } = useSaveData();
+
+  const exportSaves = () => {
+    Clipboard.setString(JSON.stringify(saveData));
+    setModalText(
+      `All the data has been copied to the clipboard. Go and paste it somewhere safe.`
+    );
+    setVisible2(true);
+  };
+
+  const importSaves = () => {
+    setModalText("This feature is coming soon!");
+    setVisible2(true);
+  };
+
+  const showPrivacyPolicy = () => {
+    setModalText(
+      "All data entered into this app is stored locally, your information remains private and does not leave your device."
+    );
+    setVisible2(true);
+  };
+
   const settingsOptions: SettingsOptionProps[] = [
-    { id: "1", title: "Import", icon: "download" },
-    { id: "2", title: "Export", icon: "upload" },
-    { id: "3", title: "Privacy Policy", icon: "user-secret" },
-    // Add more settings options as needed
+    {
+      id: "1",
+      title: "Import",
+      icon: "download",
+      settingsFunction: () => {
+        importSaves();
+      },
+    },
+    {
+      id: "2",
+      title: "Export",
+      icon: "upload",
+      settingsFunction: () => {
+        exportSaves();
+      },
+    },
+    {
+      id: "3",
+      title: "Privacy Policy",
+      icon: "user-secret",
+      settingsFunction: () => {
+        showPrivacyPolicy();
+      },
+    },
   ];
 
   const renderSettings = ({ item }: { item: SettingsOptionProps }) => (
-    <SettingsOption title={item.title} id={item.id} icon={item.icon} />
+    <SettingsOption
+      title={item.title}
+      id={item.id}
+      icon={item.icon}
+      settingsFunction={item.settingsFunction}
+    />
   );
 
   return (
@@ -53,6 +111,11 @@ const MoreModal: React.FC<MoreModalProps> = ({ visible, setVisible }) => {
       <StatusBar
         barStyle="light-content"
         backgroundColor={Colors.theme.Pakistan}
+      />
+      <CustomModal
+        text={modalText}
+        visible={visible2}
+        setVisible={setVisible2}
       />
 
       <View style={styles.modalContainer}>
@@ -71,7 +134,7 @@ const MoreModal: React.FC<MoreModalProps> = ({ visible, setVisible }) => {
             size={40}
           />
         </Pressable>
-        <Text style={styles.title}>Settings and Stuff</Text>
+        <Text style={styles.title}>Settings</Text>
         <FlatList
           data={settingsOptions}
           renderItem={renderSettings}
@@ -101,7 +164,7 @@ const styles = StyleSheet.create({
     color: Colors.theme.EarthYellow,
     fontSize: 24,
     alignSelf: "center",
-    marginTop: 20,
+    marginTop: 30,
     fontWeight: "bold",
   },
   settingsOptionContainer: {
